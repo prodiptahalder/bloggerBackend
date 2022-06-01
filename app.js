0a8c39d729dd7099e2e556766d24bc4e74b379d2
Prodipta <haldip9500@gmail.com>
2022-06-01 22:34:02 +0530

    default route added

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ app.use('*', (req, res, next) => {
 
 //MIDDLEWARE
 
+app.get('/', (req, res)=>{res.send(`Welcome to Blogger REST API Service \n Documentation: https://github.com/prodiptahalder/bloggerBackend/tree/main/API/Documentation`);});
 
 //ROUTES
 
