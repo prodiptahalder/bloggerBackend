9fc8171c62a79b4bcb079b78eaa7be8f4d92279e
Prodipta <haldip9500@gmail.com>
2022-06-01 21:56:15 +0530

    controller error fixed

--- API/Routes/user.js
+++ API/Routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {createUser, logIn} = require('../controllers/user');
+const {createUser, logIn} = require('../Controllers/user');
 
 //create user using sign up
 router.post('/signup', createUser);
